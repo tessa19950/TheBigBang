c27efc452f6c2b207c7b33b72128e9ed98bbb1d7
tessa19950 <tessalaurijssens@gmail.com>
2022-06-13 20:04:09 +0200

    add new quests and update the questlines

--- src/main/java/com/homebrewCult/TheBigBang/gui/quests/EnumQuestItem.java
+++ src/main/java/com/homebrewCult/TheBigBang/gui/quests/EnumQuestItem.java
@@ -4,10 +4,22 @@ import net.minecraft.util.IStringSerializable;
 
 public enum EnumQuestItem implements IStringSerializable {
 
-	None("Untitled Quest Item", 30),
-	BlackbullsDeedOfTheLand("Blackbull's Deed of the Land", 30),
-	AncientScroll("Ancient Scroll", 30),
-	AntiqueScroll("Antique Scroll", 30);
+	None("Untitled Quest Item", 1),
+	BlackbullsDeed("Blackbull's Deed of the Land", 1),
+	AncientScroll("Ancient Scroll", 1),
+	AntiqueScroll("Antique Scroll", 1),
+	RolyPolysTools("Roly Poly's Tools", 1),
+	OctopusMail("Octopus' Private Mail", 1),
+	VaccineRecipe("Vaccine Recipe", 1),
+	NotBreathingPotionRecipe("Recipe for Potion of Not-Breating", 1),
+	PackagedFrog("Packaged Experimental Frog", 1),
+	LostBook("Lost Book", 1),
+	MissingReport("Missing Research Report", 1),
+	ManjisDiary("Manji's Old Diary", 1),
+	TranslationGuide("Horrible Handwritings Translation Guide", 1),
+	JrYetiKeychain("Jr. Yeti Keychain", 1),
+	LucasLetter("Letter for Chief Lucas", 1),
+	SnailLegend("Legend of Mano the Snail", 1);
 	
 	private final String questItemName;
 	private final float dropPercentage;
--- src/main/java/com/homebrewCult/TheBigBang/gui/quests/Questline.java
+++ src/main/java/com/homebrewCult/TheBigBang/gui/quests/Questline.java
@@ -14,43 +14,53 @@ public enum Questline implements IStringSerializable {
 	new Biome[] {}),
 	
 	Octopuses("Octopuses", new EntityType<?>[] {ModEntities.OCTOPUS_ENTITY}, 
-	new Quest[] {Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.MrsMingMingsWorry, Quest.MakingASparklingRockOctopus, Quest.KerningCitizenRequest, Quest.HowToSurvive, Quest.BuildingANewHouseOctopus,
+			Quest.Polluted2, Quest.TheDarkLordsTraining, Quest.OctopusesAreHealthy, Quest.RolyPolys6sTools, Quest.OctopusAreAliens, Quest.VirusSampleResearchOctopus},
 	new Biome[] {Biomes.GIANT_SPRUCE_TAIGA, Biomes.GIANT_SPRUCE_TAIGA_HILLS, Biomes.GIANT_TREE_TAIGA, Biomes.GIANT_TREE_TAIGA_HILLS, Biomes.TAIGA, Biomes.TAIGA_HILLS, Biomes.BEACH}),
 	
 	Eyes("Eye Monsters", new EntityType<?>[] {ModEntities.EVIL_EYE_ENTITY, ModEntities.CURSE_EYE_ENTITY, ModEntities.COLD_EYE_ENTITY}, 
-	new Quest[] {Quest.WhatPiaHasBorrowed, Quest.Polluted, Quest.LazyLittleCalico, Quest.ArwensPaint, Quest.StrangeDish_EvilEye, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.StrangeDish_EvilEye, Quest.WhatPiaHasBorrowed, Quest.LazyLittleCalico, Quest.ArwensPaint, Quest.StrangeDish_CurseEye,
+			Quest.Polluted2, Quest.AlexTheRunawayKid,  Quest.CutthroatMannysRequest, Quest.StrangeDish_ColdEye, Quest.KerningCityBookStore},
 	new Biome[] {Biomes.SWAMP, Biomes.SWAMP_HILLS, Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE, Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS}),
 	
 	Mushrooms("Mushrooms", new EntityType<?>[] {ModEntities.ORANGE_MUSHROOM_ENTITY, ModEntities.BLUE_MUSHROOM_ENTITY}, 
-	new Quest[] {Quest.ANewHouseForBlackbull, Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.BiggsItemCollection, Quest.BarisTest, Quest.PiaAndTheBlueMushroom, Quest.TheMushroomStudies,
+			Quest.Polluted2, Quest.BuildingANewHouseMushroom, Quest.MushroomJuiceMixer, Quest.MushroomSaladRecipe, Quest.SaladIngredients, Quest.MakingHornyMushroomPie},
 	new Biome[] {Biomes.PLAINS, Biomes.FOREST, Biomes.BIRCH_FOREST, Biomes.BIRCH_FOREST_HILLS, Biomes.TALL_BIRCH_FOREST, Biomes.TALL_BIRCH_HILLS, Biomes.MUSHROOM_FIELD_SHORE, Biomes.MUSHROOM_FIELDS}),
 	
 	ZombieMushrooms("Zombie Mushrooms", new EntityType<?>[] {ModEntities.ZOMBIE_MUSHROOM_ENTITY}, 
-	new Quest[] {Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.ZombieMushroomSignal, Quest.TheOutbreak, Quest.RowensCursedMushrooms, Quest.MysteryOfTheHospital, Quest.MakingSparklingRockUndeadMushroom,
+			Quest.Polluted2, Quest.ShawnTheExcavator, Quest.HowToAvoidTheStink, Quest.SpiritDiviner, Quest.GasMaskAlternative, Quest.ZombieMushroomThreat},
 	new Biome[] {Biomes.DARK_FOREST, Biomes.DARK_FOREST_HILLS}),
 	
 	Drakes("Drakes", new EntityType<?>[] {ModEntities.DRAKE_ENTITY, ModEntities.COPPER_DRAKE_ENTITY, ModEntities.DARK_DRAKE_ENTITY}, 
-	new Quest[] {Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.WhatPiaHasBorrowed, Quest.BuildingMoreHouses, Quest.JanesChallenge, Quest.JanesLastExperiment, Quest.AClueToTheSecretBook,
+			Quest.Polluted2, Quest.TransportingDrakeSkulls, Quest.FossilResearchProgress, Quest.SealUpACriticalDanger, Quest.MuirhatsWorry, Quest.ManjisTale},
 	new Biome[] {Biomes.SAVANNA, Biomes.SAVANNA_PLATEAU, Biomes.SHATTERED_SAVANNA, Biomes.SHATTERED_SAVANNA_PLATEAU}),
 	
 	Golems("Golems", new EntityType<?>[] {ModEntities.STONE_GOLEM_ENTITY, ModEntities.DARK_STONE_GOLEM_ENTITY, ModEntities.MIXED_GOLEM_ENTITY}, 
-	new Quest[] {Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.DrBrucesResearch, Quest.UnknownBlueMaterial, Quest.ANewHouseForBlackbull, Quest.LastMaterialForRonnie, Quest.MayasLastCollection,
+			Quest.Polluted2, Quest.CrackedRubble, Quest.EliminateTheGolems, Quest.TheMixedGolemThreat, Quest.TheMixedGolemMutation, Quest.CharmsForInfo},
 	new Biome[] {Biomes.GRAVELLY_MOUNTAINS, Biomes.MODIFIED_GRAVELLY_MOUNTAINS, Biomes.MOUNTAIN_EDGE, Biomes.MOUNTAINS, Biomes.TAIGA_MOUNTAINS, Biomes.WOODED_MOUNTAINS}),
 	
 	Snails("Snails", new EntityType<?>[] {ModEntities.GREEN_SNAIL_ENTITY, ModEntities.BLUE_SNAIL_ENTITY, ModEntities.RED_SNAIL_ENTITY}, 
-	new Quest[] {Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.BasicTraining, Quest.MaisTraining, Quest.HelpHuntTheSnails, Quest.MariasLetter, Quest.HelpOutYoona,
+			Quest.Polluted2, Quest.MaisSecondTraining, Quest.MayasFirstCollection, Quest.TheOldSnail, Quest.ARainbowShell, Quest.BobTheSnail},
 	new Biome[] {Biomes.FLOWER_FOREST, Biomes.SUNFLOWER_PLAINS}),
 	
 	JrYetis("Jr. Yetis", new EntityType<?>[] {ModEntities.JRYETI_ENTITY, ModEntities.DARK_JRYETI_ENTITY}, 
-	new Quest[] {Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.GordonTheCraftsman, Quest.BeastTamer, Quest.OneMoreWish, Quest.GordonsTuition,
+			Quest.Polluted2, Quest.BettysResearch, Quest.JrYetiKeychain, Quest.YetiResearchProgress},
 	new Biome[] {Biomes.SNOWY_BEACH, Biomes.SNOWY_MOUNTAINS, Biomes.SNOWY_TAIGA, Biomes.SNOWY_TAIGA_HILLS, Biomes.SNOWY_TAIGA_MOUNTAINS, Biomes.SNOWY_TUNDRA}),
 	
 	Yetis("Yetis", new EntityType<?>[] {ModEntities.YETI_ENTITY, ModEntities.DARK_YETI_ENTITY}, 
-	new Quest[] {Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted, Quest.ReawakeningTheGladius, Quest.SnowfieldGiant, Quest.SonOfScadurTheHunter, Quest.BlakesBiggestFan,
+			Quest.Polluted2, Quest.MarchOfTheYetiArmy, Quest.YetiRides, Quest.ASymbioticRelationship},
 	new Biome[] {Biomes.ICE_SPIKES, Biomes.DEEP_FROZEN_OCEAN, Biomes.FROZEN_OCEAN, Biomes.FROZEN_RIVER}),
 	
 	Stumps("Stumps", new EntityType<?>[] {ModEntities.STUMP_ENTITY, ModEntities.DARK_STUMP_ENTITY, ModEntities.AXE_STUMP_ENTITY, ModEntities.DARK_AXE_STUMP_ENTITY}, 
-	new Quest[] {Quest.Polluted, Quest.Polluted2},
+	new Quest[] {Quest.Polluted,
+			Quest.Polluted2},
 	new Biome[] {Biomes.DESERT, Biomes.DESERT_HILLS, Biomes.DESERT_LAKES, Biomes.BADLANDS, Biomes.BADLANDS_PLATEAU, Biomes.ERODED_BADLANDS, Biomes.MODIFIED_BADLANDS_PLATEAU, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.WOODED_BADLANDS_PLATEAU});
 	
 	private final String DISPLAY_NAME;
